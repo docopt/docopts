72d7d797dfd846ecb16ee01f4305c72b1877e679
Lari Rasku <raskug@lavabit.com>
2013-02-07 00:51:59 +0200

    Removed useless file_get_contents function
    setup.py doesn't work anyway when called outside the package directory

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
 from setuptools import setup
-
-def file_get_contents(name):
-    with open(os.path.join(os.path.dirname(__file__), name)) as f:
-        return f.read()
 
 setup(name             = "docopts",
       version          = "0.6.1",
@@ -16,7 +11,7 @@ setup(name             = "docopts",
       description      = "Shell interface for docopt, the command-line "
                          "interface description language.",
       keywords         = "shell bash docopt command-line",
-      long_description = file_get_contents('README.rst'),
+      long_description = open('README.rst').read(),
       scripts          = ["docopts"],
       install_requires = ["docopt == 0.6.1"],
       classifiers      = ["Development Status :: 3 - Alpha",
