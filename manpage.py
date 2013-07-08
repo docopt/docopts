0beb12a2fb34db5f7547845bb72da02fcd0a3890
Lari Rasku <lari.o.rasku@gmail.com>
2013-07-08 04:39:55 +0300

    Backup some more work

--- manpage.py
+++ manpage.py
@@ -35,26 +35,9 @@ class install(Command):
         if not self.dry_run:
             pass
 
+class Section(str):
+    
+
 def stripindent(string):
     return (sum(1 for s in itertools.takewhile(str.isspace, string)),
             string.lstrip())
-
-def convert(source):
-    lines = source.expandtabs().splitlines()
-    header = lines.pop(0)
-    footer = lines.pop(-1)
-    
-    iterator = iter(lines)
-    indent, line = stripindent(iterator.next())
-    for lookahead in iterator:
-        lookahead_indent, lookahead_line = stripindent(lookahead)
-        if lookahead_indent > indent > 0:
-            pass
-
-def setup():
-    pass
-
-if __name__ == '__main__':
-    import sys
-    with open(sys.argv[1]) as f:
-        print(convert(f.read()))
