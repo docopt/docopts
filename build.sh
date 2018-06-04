612f9c5c636f60c130d33edb3ccde720f6fd3f95
Sylvain Viart <sylvain303@github>
2018-06-04 18:49:57 +0200

    add build scripts used to produce firt release.
    See: https://github.com/Sylvain303/docopts/releases/tag/v0.6.3-alpha1

--- build.sh
+++ build.sh
@@ -1,50 +1,15 @@
 #!/bin/bash
 #
-# Build embedded docopt.py into docopts.sh
+# Build docopts go version
+#
 
 # exit on error
 set -e
 
-echo "no build"
-exit 0
+go get github.com/docopt/docopt-go
 
-echo "download: docopt.py form github…"
-wget --no-check-certificate -O docopt.py "https://raw.githubusercontent.com/docopt/docopt/master/docopt.py"
+# build 64 bits version
+go build docopts.go
 
-[[ -s docopt.py ]]
-
-sourcef=docopts.sh
-embedded_mark='### EMBEDDED'
-
-echo "merging docopt.py + docopts.py"
-# remove duplicate import and code
-sed -i -e '/^import sys/ d' \
-    -e '/^import re/ d' \
-    -e '/^__all__ =/ d' \
-    -e '/^__version__ =/ d' \
-    docopt.py
-
-# replace import
-# from docopt import docopt, DocoptExit, DocoptLanguageError
-sed -e '/^from docopt/ {
-   s//# embedded: &/
-   r docopt.py
-   a\
-# ----------------------- end docopt.py ---------------------------
-   }' docopts > docopts.py
-
-# check if code is modified
-s=$(git status -uno --short $sourcef)
-if [[ ! -z "$s" ]] ; then
-    echo $s
-    echo "$sourcef is modified, commit or revert first"
-    exit 1
-fi
-echo "removing embedded code behind '$embedded_mark'"
-sed -i -e "/^$embedded_mark/,\$ d" $sourcef
-echo "add marker and reembbed code"
-echo "$embedded_mark" >> $sourcef
-sed -e "s/^/#> /" docopts.py >> $sourcef
-# remove ending blanks there's a TAB + space in the [] below
-sed -i -e 's/[ 	]\+$//g' $sourcef
-
+# build 32 bits version too
+env GOOS=linux GOARCH=386 go build -o docopts-32bits docopts.go
