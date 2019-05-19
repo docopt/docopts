73bde770686815fae937e2858551719acde01b17
Richard Donkin <richard@donkin.org>
2019-05-19 10:42:04 +0100

    examples for bash 3.x
    - move sshdiff.sh to main examples - depends on bash 4.x

---
+++ examples/legacy_bash/cat-n_wrapper_example.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# cat -n all files
+#
+# Usage: cat-n_wrapper_example.sh [--count=N] FILE...
+#
+# Arguments:
+#   FILE     input file
+#
+# Options:
+#   --count=N   limit the number of line to display
+#
+# Examples:
+#    ./cat-n_wrapper.sh --count=3 cat-n_wrapper.sh  quick_example.sh
+#
+
+# no PATH changes required if docopts binary is in the PATH already
+PATH=..:$PATH
+source ../docopts.sh
+help=$(docopt_get_help_string $0)
+version='0.1'
+
+parsed=$(docopts -G args -h "$help" -V $version : "$@")
+# Show parsed arguments
+#echo "$parsed"
+eval "$parsed"
+
+cat_limit() {
+    if [[ -z "${args[--count]}" ]] ; then
+        cat -n "$1"
+    else
+        cat -n "$1" | head -"${args[--count]}"
+    fi
+}
+
+# current docopts multiple argument wrapper
+n=${args[FILE,#]}
+for i in $(seq 0 $(($n - 1)))
+do
+    f="${args[FILE,$i]}"
+    echo "----- $f -------"
+    cat_limit  "$f"
+done
