51366eb3839e7e34c8207e0610b41f4e09dc314c
Richard Donkin <richard@donkin.org>
2019-05-19 10:42:04 +0100

    examples for bash 3.x
    - move sshdiff.sh to main examples - depends on bash 4.x

---
+++ examples/legacy_bash/naval_fate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Naval Fate.
+#
+# Usage:
+#   naval_fate.sh ship new <name>...
+#   naval_fate.sh ship <name> move <x> <y> [--speed=<kn>]
+#   naval_fate.sh ship shoot <x> <y>
+#   naval_fate.sh mine (set|remove) <x> <y> [--moored|--drifting]
+#   naval_fate.sh -h | --help
+#   naval_fate.sh --version
+#
+# Options:
+#   -h --help     Show this screen.
+#   --version     Show version.
+#   --speed=<kn>  Speed in knots [default: 10].
+#   --moored      Moored (anchored) mine.
+#   --drifting    Drifting mine.
+#
+
+
+# if docopts is in PATH, not needed.
+# Note: docopts.sh is also found in PATH
+PATH=..:$PATH
+
+VERSION='Naval Fate 2.0'
+source docopts.sh
+# no version support in docopt_auto_parse() so we call docopts directly
+usage=$(docopt_get_help_string "$0")
+parsed="$(docopts -G ARGS -V "$VERSION" -h "$usage" : "$@")"
+echo "$parsed"
+eval "$parsed"
+
+# docopt_print_ARGS
