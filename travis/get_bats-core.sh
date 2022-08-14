6360b3bd8d9670b7250eb5066819048263b784fc
Anton Gilgur <agilgur5@gmail.com>
2022-08-13 21:43:20 -0400

    optim(ci): clone `bats-core` with `--depth=1`
    - should be a good bit faster than cloning with the whole history

--- travis/get_bats-core.sh
+++ travis/get_bats-core.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/bats-core/bats-core.git
+git clone https://github.com/bats-core/bats-core.git --depth=1
 cd bats-core
 sudo ./install.sh /usr/local
 
